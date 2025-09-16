629a790eea2e3eec2ab7fcf659c2c97f9b7d9bd7
NicoJordani <nicolasjordani5@gmail.com>
2025-09-16 18:51:08 -0300

    alterei apenas a pasta as_sam_d21

--- rtos/as_sam_d21/src/main.c
+++ rtos/as_sam_d21/src/main.c
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_9(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -52,6 +53,7 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_9			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -65,6 +67,7 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+uint32_t PILHA_TAREFA_9[TAM_PILHA_9];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -80,10 +83,14 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
     
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
+	//CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
 	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
+	//CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 1);
 	
+    //CriaTarefa(tarefa_4,"Tarefa 4",PILHA_TAREFA_4,TAM_PILHA_4,2);
+    
+    CriaTarefa(tarefa_9,"Tarefa 9",PILHA_TAREFA_9,TAM_PILHA_9,3);
+    
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
 	
@@ -100,6 +107,22 @@ int main(void)
 }
 
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
+void tarefa_9(void){
+
+    volatile uint16_t cont=0;
+    
+    for(;;){
+    
+        cont++;
+        TarefaEspera(500);
+    }
+
+}
+
+
+
+
+
 void tarefa_1(void)
 {
 	volatile uint16_t a = 0;
